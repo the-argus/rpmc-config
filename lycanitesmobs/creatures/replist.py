4ab90fca29e9ce285bd883ef821d8db8734e9b80
Ian McFarlane <ifm1110@rit.edu>
2021-12-08 19:18:49 -0500

    init commit

---
+++ lycanitesmobs/creatures/replist.py
@@ -0,0 +1,68 @@
+import json
+import sys
+import os
+
+keeplist = [
+        "grue",
+        "sylph",
+        "wisp",
+        "cinder",
+        "kobold",
+        "yale",
+        "yeti",
+        "warg",
+        "feradon",
+        "quillbeast",
+        "maug",
+        "wraamon",
+        "ent",
+        "shambler",
+        "ventoraptor",
+        "raiko",
+        "frostweaver",
+        "tarantula",
+        "darkling",
+        "herma",
+        "troll",
+        "lobber",
+        "morock",
+        "remobra",
+        "crusk",
+        "serpix",
+        "gorger",
+        "skylus",
+        "cephignus",
+        "roa",
+        "ioray",
+        "beholder",
+        "reaper",
+        "geist",
+        "wendigo",
+        "cryptkeeper",
+        "ghoul",
+        "pinky",
+        "nymph"
+]
+
+path = os.getcwd()
+
+def main():
+    for root, subs, files in os.walk(path):
+        for file in files:
+            data = {}
+            if file[-5:] == ".json":
+                with open(file, "r") as f:
+                    data = json.load(f)
+                if file[:-5] in keeplist:
+                    data["enabled"] = True
+                else:
+                    data["enabled"] = False
+                data["loadDefault"] = False
+                json.dump(data, open(file, "w"), indent=4)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
