e46dfc35dcde73a381ab2bb413ddca4b0db5231d
Ian McFarlane <ifm1110@rit.edu>
2021-12-11 15:47:17 -0500

    attempted fix at disabled mob spawners

--- lycanitesmobs/creatures/replist.py
+++ lycanitesmobs/creatures/replist.py
@@ -3,45 +3,14 @@ import sys
 import os
 
 keeplist = [
-        "grue",
-        "sylph",
-        "wisp",
-        "cinder",
-        "kobold",
-        "yale",
-        "yeti",
-        "warg",
-        "feradon",
-        "quillbeast",
-        "maug",
-        "wraamon",
-        "ent",
-        "shambler",
-        "ventoraptor",
-        "raiko",
-        "frostweaver",
-        "tarantula",
-        "darkling",
-        "herma",
-        "troll",
-        "lobber",
-        "morock",
-        "remobra",
-        "crusk",
-        "serpix",
-        "gorger",
-        "skylus",
-        "cephignus",
-        "roa",
-        "ioray",
-        "beholder",
-        "reaper",
-        "geist",
-        "wendigo",
-        "cryptkeeper",
-        "ghoul",
-        "pinky",
-        "nymph"
+        "grue", "sylph", "wisp", "cinder", "kobold",
+        "yale", "yeti", "warg", "feradon", "quillbeast", 
+        "maug", "wraamon", "ent", "shambler", "ventoraptor",
+        "raiko", "frostweaver", "tarantula", "darkling", "herma",
+        "troll", "lobber", "morock", "remobra", "crusk", 
+        "serpix", "gorger", "skylus", "cephignus", "roa",
+        "ioray", "beholder", "reaper", "geist", "wendigo",
+        "cryptkeeper", "ghoul", "pinky", "nymph"
 ]
 
 path = os.getcwd()
@@ -53,10 +22,17 @@ def main():
             if file[-5:] == ".json":
                 with open(file, "r") as f:
                     data = json.load(f)
+#                    print(data)
+#                    break
                 if file[:-5] in keeplist:
                     data["enabled"] = True
                 else:
                     data["enabled"] = False
+                    try:
+                        data["spawning"]["enabled"] = False
+                    except KeyError:
+                        print(f"{file} is missing either the spawning or spawning.enabled key. skipping")
+                        continue
                 data["loadDefault"] = False
                 json.dump(data, open(file, "w"), indent=4)
 
