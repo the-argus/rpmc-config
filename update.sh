4312308a14550795518c6460bae7055d66387876
Ian McFarlane <ifm1110@rit.edu>
2021-12-19 16:23:13 -0600

    misc changes

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -av --exclude=".*" /home/argus/Desktop/media/Games/Minecraft/Modpacks/RPMC/config-changes/* "/home/argus/.local/share/multimc/instances/compat testing/.minecraft/config"
+rsync -av --exclude=".*" /home/argus/Desktop/Minecraft/Modpacks/RPMC/config-changes/* "/home/argus/.local/share/multimc/instances/compat testing/.minecraft/config"
