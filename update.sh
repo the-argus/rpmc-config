809db5eca01df3514ce55a0a65d0fed9ac40ba3a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-02-18 11:01:20 -0500

    fixed path formatting

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-INSTANCE_PATH="$HOME/.local/share/multimc/instances/Ian's RPMC/.minecraft/config"
+INSTANCE_PATH="$HOME/.local/share/multimc/instances/RPMC/.minecraft/config"
 rsync -av --exclude=".*" ./* $INSTANCE_PATH
